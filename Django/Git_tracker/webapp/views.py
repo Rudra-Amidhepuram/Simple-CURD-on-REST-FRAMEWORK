9fea5884957e6cfde46d26f3db7a36acda1a492f
Rudra-Amidhepuram <52567267+Rudra-Amidhepuram@users.noreply.github.com>
2019-07-07 16:00:12 +0530

    Update views.py

--- Django/Git_tracker/webapp/views.py
+++ Django/Git_tracker/webapp/views.py
@@ -9,37 +9,41 @@ from rest_framework import status
 from django.http import Http404
 from rest_framework.views import APIView
 from django_filters.rest_framework import DjangoFilterBackend
-# queryset = user.objects.all()
-#serializer_class = userList
 
+# view class for Repo 
 class RepoView(generics.ListAPIView):
 	serializer_class=RepoSerializer
 	queryset=Repo.objects.all()
+
+# view class for Event of a paricular repo 
 class Repo_events(generics.ListAPIView):
 	serializer_class=EventSerializer
 	queryset=Event.objects.all()
 	filter_backends= (DjangoFilterBackend,)
 	filter_fields=('r_id', )
 
+# view class for Events 
 class EventsViews(generics.ListAPIView):
 	serializer_class=EventSerializer
 	queryset=Event.objects.all()
 
+# view class for Actors 
 class ActorViews(generics.ListAPIView):
 	serializer_class=ActorSerializer
 	queryset=Actor.objects.all()
-	
+
+# view class for Actors of a particular Event 
 class Actor_events(generics.ListAPIView):
 	serializer_class=ActorSerializer
 	queryset=Actor.objects.all()
 	filter_backends= (DjangoFilterBackend,)
 	filter_fields=('e_id', )
-
+# view class to delete/update a particular Event 
 class update_delete_event(generics.RetrieveUpdateDestroyAPIView):
     queryset = Event.objects.all()
     serializer_class = serializers.EventSerializer
 
-
+# view class to add event
 class Add_event(generics.ListCreateAPIView):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
