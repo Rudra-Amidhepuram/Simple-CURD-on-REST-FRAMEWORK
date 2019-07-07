ba750ba75bea1ec627b5925716e292ec39baecb7
Rudra-Amidhepuram <52567267+Rudra-Amidhepuram@users.noreply.github.com>
2019-07-07 15:56:19 +0530

    Update admin.py

--- Django/Git_tracker/webapp/admin.py
+++ Django/Git_tracker/webapp/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import	user,Repo,Event,Actor
+from .models import	Repo,Event,Actor
 
-admin.site.register(user)
 admin.site.register(Repo)
 admin.site.register(Event)
 admin.site.register(Actor)
