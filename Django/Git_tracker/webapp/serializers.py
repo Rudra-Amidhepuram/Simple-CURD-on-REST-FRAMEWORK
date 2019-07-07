1b45caca1eeb361e7f1471a1c512edde4ccba08a
Rudra-Amidhepuram <52567267+Rudra-Amidhepuram@users.noreply.github.com>
2019-07-07 15:59:14 +0530

    Update serializers.py

--- Django/Git_tracker/webapp/serializers.py
+++ Django/Git_tracker/webapp/serializers.py
@@ -1,26 +1,23 @@
 from rest_framework import serializers
-from .models import user,Repo,Event,Actor
+from .models import Repo,Event,Actor
 
-class userList(serializers.ModelSerializer):
-
-	class Meta:
-		model=user
-		fields=('id','name','p_name','mobile')
-
-
+# serializer for Repo model
 class RepoSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Repo
 		fields= '__all__'
 
-
+# serializer for Event model
 class EventSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Event
 		fields= '__all__'
 
+
+# serializer for Actor model
 class ActorSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Actor
 		fields= '__all__'
-
+		read_only_fields= ('e_id','r_id', ) 
+		depth=3   #it displays the content of "e_id" and "r_id"
