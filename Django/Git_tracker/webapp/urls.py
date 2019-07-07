ad85da2ed3a9e2ed314f6c7f036f90d0c8e94dac
Rudra-Amidhepuram <52567267+Rudra-Amidhepuram@users.noreply.github.com>
2019-07-07 16:01:54 +0530

    Update urls.py

--- Django/Git_tracker/webapp/urls.py
+++ Django/Git_tracker/webapp/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     
 	path('repo/', RepoView.as_view(), name="repo-all"),
 	path('rep/', Repo_events.as_view(), name="repo_events-all"),
-	path('evt/', Actor_events.as_view(), name="Actor_events-all"),
+	path('actor_events/', Actor_events.as_view(), name="Actor_events-all"),
 	path('events/',EventsViews.as_view(), name="ALL Events"),
 	path('actors/',ActorViews.as_view(), name="ALL Actros"),
-    path('event/<int:pk>/', update_delete_event.as_view()),
+        path('event/<int:pk>/', update_delete_event.as_view()),
 	path('add/',Add_event.as_view())
 	
 ]
