511102b29b28b4cc619e6de737c02acabfdd0ae8
Rudra-Amidhepuram <52567267+Rudra-Amidhepuram@users.noreply.github.com>
2019-07-07 16:37:44 +0530

    Update urls.py

--- Django/Git_tracker/Git_tracker/urls.py
+++ Django/Git_tracker/Git_tracker/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, re_path, include
+from webapp.views import RepoView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', include('webapp.urls')),
+	re_path('', RepoView.as_view()),
 ]
